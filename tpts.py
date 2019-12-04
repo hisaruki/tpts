28e74c34b47716301331bb0a78def9ff8b20615a
hisaruki <hisaruki@gmail.com>
2019-12-04 10:15:16 +0900

    largeから

--- tpts.py
+++ tpts.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import argparse,requests,re,os
+import argparse
+import requests
+import re
+import os
 from pathlib import Path
 from django.utils.http import parse_http_date
 
@@ -10,23 +13,23 @@ parser.add_argument("path")
 args = parser.parse_args()
 
 p = Path(args.path)
-m = re.match(r'(^[^\.]*)\.(.*$)',str(p.name))
+m = re.match(r'(^[^\.]*)\.(.*$)', str(p.name))
 filename = m.group(1)
-ext = re.sub("[\-\.].*","",m.group(2))
-url = 'https://pbs.twimg.com/media/' + filename + "." + ext + ":large"
-
+ext = re.sub("[\-\.].*", "", m.group(2))
+ext = ext.replace('_large', '')
+url = 'https://pbs.twimg.com/media/' + filename + "." + ext + ":orig"
+print(url)
 r = requests.head(url)
 if "Last-Modified" in r.headers:
-  lm = parse_http_date(r.headers["Last-Modified"])
-  cl = int(r.headers["Content-Length"])
+    lm = parse_http_date(r.headers["Last-Modified"])
+    cl = int(r.headers["Content-Length"])
 
-  if p.stat().st_size < cl:
-    r = requests.get(url)
-    with p.open("wb") as f:
-      f.write(r.content)
+    if p.stat().st_size < cl:
+        r = requests.get(url)
+        with p.open("wb") as f:
+            f.write(r.content)
 
-  nf = p.parent / Path(filename + "." + ext + "-large." + ext)
+    nf = p.parent / Path(filename + "." + ext + "-orig." + ext)
 
-  p.rename(nf)
-  os.utime(str(nf),(lm,lm))
-  print(nf)
+    p.rename(nf)
+    os.utime(str(nf), (lm, lm))
